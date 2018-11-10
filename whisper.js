07f5fe36189a0dbe367a5dd9d355290bd995c89d
cglotr <amir.ariffin.920404@gmail.com>
2018-11-10 15:10:10 +0800

    x & y movement based on the instruction

--- whisper.js
+++ whisper.js
@@ -1,3 +1,28 @@
-export default function(question){
-  return {end: question.start}
+export default function (question) {
+  let endX = question.startX
+  let endY = question.startY
+
+  question.instructions.split('').forEach(m => {
+    switch (m) {
+      case 'F':
+        endX++
+        break
+      case 'B':
+        endX--
+        break
+      case 'L':
+        endY++
+        break
+      case 'R':
+        endY--
+        break
+      default:
+        break
+    }
+  })
+
+  return {
+    endX,
+    endY
+  }
 }
