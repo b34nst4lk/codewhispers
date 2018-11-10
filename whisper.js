ca204763a6cd8c273b77d7b7a9ab8532ddd97b42
Jack <jsim89@gmail.com>
2018-11-10 20:22:30 +0800

    Round 6 | working solution

--- whisper.js
+++ whisper.js
@@ -1,49 +1,98 @@
 export default function (question) {
+  console.log(question)
   let endX = question.startX
   let endY = question.startY
+
   let treasureX = question.treasureX
   let treasureY = question.treasureY
   let treasureFound = false
-  let pirateX = question.pirateX
-  let pirateY = question.pirateY
   let treasureStolen = false
 
-  function treasureCheck() {
-    if (endX == treasureX && endY == treasureY) {
-      treasureFound = true
-    }
-  }
+  let pirateX = question.pirateX
+  let pirateY = question.pirateY
+  let pirateEncountered = false
 
-  function pirateCheck() {
-    if (endX == pirateX && treasureFound == true)
-    treasureStolen = true;
-  }
+  let spyX = question.spyX
+  let spyY = question.spyY
+  let spyFollowing = false
 
-  question.instructions.split('').forEach(m => {
+  let spyAndPirateFought= false
+  let treasureOwner = "no-one"
+
+  let events = {}
+
+  function move(m) {
     switch (m) {
       case 'F':
-      endX++
-      break
+        endX++
+        break
       case 'B':
-      endX--
-      break
+        endX--
+        break
       case 'L':
-      endY++
-      break
+        endY++
+        break
       case 'R':
-      endY--
-      break
+        endY--
+        break
       default:
-      break
+        break
     }
-    treasureCheck()
-    pirateCheck();
+  }
+
+  function addNewEvent(i, event) {
+    if (!events[i]) events[i] = []
+    events[i].push(event)
+  }
+
+  function treasureCheck(i) {
+    if (!treasureFound && treasureX === endX && treasureY === endY) {
+      treasureFound = true
+      addNewEvent(i, 'treasure found')
+      treasureOwner = "me"
+    }
+  }
+
+  function spyCheck(i) {
+    if (!spyFollowing && !treasureFound && spyX === endX && spyY === endY) {
+      spyFollowing = true
+      addNewEvent(i, 'spy following')
+    } 
+    if (spyFollowing && treasureFound) {
+      treasureOwner = "spy"   
+    }
+  }
+
+  function pirateCheck(i) {
+    if (!spyAndPirateFought && pirateX === endX && pirateY === endY) {
+      pirateEncountered = true
+      addNewEvent(i, 'pirates encountered')
+      if (spyFollowing && treasureFound) {
+        addNewEvent(i, 'fight!')
+        spyAndPirateFought = true
+      } else if (treasureFound) {
+        treasureStolen = true
+      }
+    }
+  }
+
+  question.instructions.split('').forEach((m,i) => {
+    move(m);
+    spyCheck(i);
+    pirateCheck(i);
+    treasureCheck(i);
   })
+
+  if (!treasureFound) treasureOwner = "no-one"
+  if (spyFollowing && treasureStolen) treasureOwner = "spy"
+  if (pirateEncountered && treasureStolen) treasureOwner = 'pirate'
+  if (spyAndPirateFought) treasureOwner = "me"
+
+  console.log(events)
  
   return {
     endX,
     endY,
-    treasureFound,
-    treasureStolen
+    treasureOwner
   }
 }
